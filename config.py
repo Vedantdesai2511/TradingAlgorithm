090086a6e9baf58571e37cef119037eacca9ddd1
Vedant <vedantdesai07@gmail.com>
2020-08-30 10:13:13 -0400

    Added configuration file

---
+++ config.py
@@ -0,0 +1,10 @@
+API_KEY = ''  # put your api Key id
+SECRET_KEY = ''
+HEADERS = {
+    'APCA-API-KEY-ID': API_KEY,
+    'APCA-API-SECRET-KEY': SECRET_KEY
+}
+BARS_URL = 'https://data.alpaca.markets/v1/bars'
+BASE_URL = 'https://paper-api.alpaca.markets'
+
+Liquidate_all_the_positions_url = 'https://paper-api.alpaca.markets/v2/positions'
