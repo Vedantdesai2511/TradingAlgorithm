9314c9759d8e35fe0e5e3a7b8b69da74bbcc4753
Vedant <vedantdesai07@gmail.com>
2020-09-01 10:50:17 -0400

    added function to liquidate all the open positions

--- Alpaca_buy_sell.py
+++ Alpaca_buy_sell.py
@@ -183,7 +183,49 @@ class AlpacaBuySell:
             if positions[idx].symbol != self.symbol:
                 return False
             else:
-
                 return True
+
+    def liqidate_positions(self):
+        """
+        This function liquidates all the open positions in the given current portfolio
+        got from the reference from internet and modified the function a bit
+        """
+
+        api = tradeapi.REST(
+            base_url=self.base_url,
+            key_id=self.api_key_id,
+            secret_key=self.api_secret
+        )
+
+        orders = api.list_orders(status='open')
+        positions = api.list_positions()
+
+        if orders or positions:
+            if positions:
+                print(positions)
+
+            if orders:
+                print("Canceling open orders:")
+                print([o.id for o in orders])
+                result = [api.cancel_order(o.id) for o in orders]
+                print(result)
+
+            closed = []
+            for p in positions:
+                side = 'sell'
+                if int(p.qty) < 0:
+                    p.qty = abs(int(p.qty))
+                    side = 'buy'
+                closed.append(
+                    api.submit_order(p.symbol, qty=p.qty, side=side, type="market", time_in_force="day")
+                )
+
+            if closed:
+                print("Submitted Orders", closed)
+
+            for o in closed:
+                status = api.get_order(o.id)
+                if status.status == 'rejected':
+                    print("ORDER FAILED: Your Order was Rejected!!!")
 
 # AlpacaBuySell("ROKU").naked_buy_order()
