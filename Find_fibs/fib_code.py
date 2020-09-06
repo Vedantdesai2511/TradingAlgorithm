928c2890394e9eca5495f362c6ae5ba2e00df746
Vedant <vedantdesai07@gmail.com>
2020-09-05 20:30:30 -0400

    cide to find fractal low for upper fib

--- Find_fibs/fib_code.py
+++ Find_fibs/fib_code.py
@@ -10,21 +10,13 @@ class UpperFibFindABCD:
         """
         i = 2
         flag_a = 0
-        # print("Yo 1")
-        if len(z) >= 5:
-            # print("length of z is bigger or equal to 5") ######################
+        if len(z) >= 5:  # if the length of input array is less grater than 5 use only last four terms of the array
+            # for the analysis
             z = z[-5:]
-        elif len(z) < 5:
-            # print("length of z is smaller than 5") ######################
+        elif len(z) < 5:  # if the length  pf the input array is less than five do no proced as to find the fractal
+            # low minimum number of points needed is 5
             return self.local_minimum_a, flag_a
 
-        if ((z[i]) - (z[i-1]) < 0) and ((z[i]) - (z[i-2]) < 0):
-            if ((z[i+1]) - (z[i]) > 0) and ((z[i+2]) - (z[i]) > 0):
-                self.local_minimum_a = z[i]
-                # print("Found up fib a")
-                # print(self.local_minimum_a)
-                flag_a = 1
-                return self.local_minimum_a, flag_a
+        # TODO: code to find fractal low
 
-        # print("no a found")
         return self.local_minimum_a, flag_a
