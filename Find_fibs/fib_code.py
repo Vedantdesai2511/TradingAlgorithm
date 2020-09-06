1080f730e06c89b0ba81257514f499ce0cf50abd
Vedant <vedantdesai07@gmail.com>
2020-09-06 11:06:40 -0400

    added a method to find fratal high after finding fractal low

--- Find_fibs/fib_code.py
+++ Find_fibs/fib_code.py
@@ -1,6 +1,7 @@
 class UpperFibFindABCD:
     def __init__(self):
         self.local_minimum_a = 0
+        self.local_maximum_b = 0
 
     def find_a(self, z):
         """
@@ -26,3 +27,25 @@ class UpperFibFindABCD:
                 return self.local_minimum_a, flag_a
 
         return self.local_minimum_a, flag_a
+
+    def find_b(self, z):
+        i = 2
+        level2 = 0
+        upper_level = 0
+        flag_a = 1
+        # print("Yo")
+        if len(z) > 5:
+            z = z[-5:]
+        else:
+            # print("Hmmm")
+            pass
+
+            if ((z[i]) - (z[i-1]) > 0) and ((z[i]) - (z[i-2]) > 0):
+                if ((z[i+1]) - (z[i]) < 0) and ((z[i+2]) - (z[i]) < 0):
+                    # print("Found up fib b")
+                    self.local_maximum_b = z[i]
+                    print(self.local_maximum_b)
+                    flag_a = 2
+
+                    return self.local_maximum_b, level2, upper_level, flag_a
+        return self.local_maximum_b, level2, upper_level, flag_a
