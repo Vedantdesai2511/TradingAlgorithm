1077967e4dffb8faf6b0657483cef0ba044a4b09
Vedant <vedantdesai07@gmail.com>
2020-09-21 22:56:16 -0400

    added code to calculate volatility in five minute chart

--- Volatility_Function_/volatility_calculation.py
+++ Volatility_Function_/volatility_calculation.py
@@ -19,10 +19,20 @@ def volatility_calculation_(stock_name_lst):
     n = 100
 
     minute_bars_url = f'{config.BARS_URL}/minute?symbols={sym}&limit={n}'
+    five_minutes_url = f'{config.BARS_URL}/5Min?symbols={sym}&limit={n}'
+    # fifteen_minute_url = f'{config.BARS_URL}/15Min?symbols={sym}&limit={n}'  # Get 15 minutes candle stick data
 
+    # day_bars_url = '{}/day?symbols={}&limit=5'.format(config.BARS_URL, sym)  # Get daily candle stick data
+
+    # r = requests.get(day_bars_url, headers=config.HEADERS)  # day candle sticks data
     r1 = requests.get(minute_bars_url, headers=config.HEADERS)
+    r5 = requests.get(five_minutes_url, headers=config.HEADERS)
+    # r15 = requests.get(fifteen_minute_url, headers=config.HEADERS)  # 15 minutes candle stick data
 
+    # data0 = r.json()
     data1 = r1.json()
+    data2 = r5.json()
+    # data3 = r15.json()
 
     def volatility_calculation(stock_name):  # This function calculates the volatility of each stock the volatility is
         # used in the fib code to decide if the trade is worth taking or not
@@ -40,6 +50,23 @@ def volatility_calculation_(stock_name_lst):
 
         load_data_test.Dict_one[stock_name]["volatility"] = volatility
         print(f'Dict_one[stock_name]["volatility"]: {load_data_test.Dict_one[stock_name]["volatility"]}')
+
+        df = pd.DataFrame.from_dict(data2[stock_name])
+        z = df["c"]
+        q = 0
+        b = np.zeros(len(z) - 1)
+        while q < len(z) - 1:
+            a = (z[q + 1] / z[q]) - 1
+            b[q] = a
+            q = q + 1
+
+        volatility = b.std() * 100
+
+        # print(f'volatility of {stock_name}: {volatility}')
+
+        load_data_test.Dict_five[stock_name]["volatility"] = volatility
+        print(f'Dict_five[stock_name]["volatility"]: of {stock_name} - '
+              f'{load_data_test.Dict_five[stock_name]["volatility"]}')
 
     def volatality_calculation_function(stock_names):
         # print("Heya")  # print statement for debugging purposes
